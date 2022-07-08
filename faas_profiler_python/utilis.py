ff364fb66b0b1e4842a9db5275307f7347af8ea0
Malte Wächter <maltewaechter1@gmail.com>
2022-07-08 23:24:56 +0200

    Add methods for base64 encoding and decoding

--- faas_profiler_python/utilis.py
+++ faas_profiler_python/utilis.py
@@ -2,8 +2,12 @@
 # -*- coding: utf-8 -*-
 
 from __future__ import annotations
+
+import json
+
 from typing import Any, Dict
 from inflection import underscore
+from base64 import b64decode, b64encode
 
 
 def get_arg_by_key_or_pos(args, kwargs, pos, kw):
@@ -33,6 +37,40 @@ def registerable_name_parts(name, delimiter: str = "::") -> tuple:
 
 def registerable_key(name, delimiter: str = "::", ) -> str:
     return "_".join(registerable_name_parts(name, delimiter))
+
+
+def decode_base64_json_to_dict(base64_json: Any) -> dict:
+    """
+    Helper method to decode a base64-encoded json object to a dict.
+
+    Details: https://docs.aws.amazon.com/lambda/latest/dg/API_Invoke.html#API_Invoke_RequestSyntax
+    """
+    try:
+        json_string = b64decode(base64_json)
+    except Exception as err:
+        raise ValueError(f"Base64 decoding bytes-like object failed: {err}")
+    else:
+        try:
+            return json.loads(json_string.decode("utf-8"))
+        except Exception as err:
+            raise ValueError(f"JSON load of encoded object failed: {err}")
+
+
+def encode_dict_to_base64_json(dict: dict) -> Any:
+    """
+    Helper method to encode a dict to a base64-encoded json object.
+
+    Details: https://docs.aws.amazon.com/lambda/latest/dg/API_Invoke.html#API_Invoke_RequestSyntax
+    """
+    try:
+        json_string = json.dumps(dict)
+    except Exception as err:
+        raise ValueError(f"Could not convert dict to json: {err}")
+    else:
+        try:
+            return b64encode(json_string.encode("utf-8")).decode("utf-8")
+        except Exception as err:
+            raise ValueError(f"Could not encode to Base64: {err}")
 
 
 class Registerable:
