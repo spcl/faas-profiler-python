b36e86f7c76a6e0d57dbcec6b8d4156997e17b88
Malte Wächter <maltewaechter1@gmail.com>
2022-07-08 23:24:01 +0200

    Add tracing in config

--- faas_profiler_python/config.py
+++ faas_profiler_python/config.py
@@ -278,3 +278,32 @@ def load_schema_by_measurement_name(
     else:
         # TODO: Log that file not found
         return {}
+
+
+"""
+Distrubted Tracing Config
+"""
+
+# https://specs.openstack.org/openstack/api-wg/guidelines/headers.html
+PROFILE_ID_HEADER = "FaaS-Profiler-Profile-ID"
+ROOT_ID_HEADER = "FaaS-Profiler-Root-ID"
+SPAN_ID_HEADER = "FaaS-Profiler-Span-ID"
+
+TRACE_CONTEXT_KEY = "_faas_profiler_context"
+
+
+@dataclass
+class TraceContext:
+    profile_id: str = None
+    root_id: str = None
+    span_id: str = None
+
+    @property
+    def is_complete(self) -> bool:
+        """
+        Returns True if trace context is complete
+        """
+        return (
+            self.profile_id is not None and
+            self.root_id is not None and
+            self.span_id is not None)
