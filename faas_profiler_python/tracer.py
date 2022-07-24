97bb3b87b486cdaf6daa08ebbe4b4884ac133ce2
Malte Wächter <git@malte-waechter.com>
2022-07-25 01:25:43 +0200

    Change tracer for new patcher design

--- faas_profiler_python/tracer.py
+++ faas_profiler_python/tracer.py
@@ -7,81 +7,13 @@ from __future__ import annotations
 
 import logging
 
-from typing import Type
-from contextlib import contextmanager
+from typing import Any, Type
 from uuid import uuid4
 
-from faas_profiler_python.config import (
-    ProfileConfig,
-    ProfileContext,
-    Provider,
-    PROFILE_ID_HEADER,
-    ROOT_ID_HEADER,
-    SPAN_ID_HEADER
-)
-from faas_profiler_python.aws import AWSInjection
+from faas_profiler_python.config import TraceContext
+from faas_profiler_python.patchers import request_patcher
+from faas_profiler_python.patchers.botocore import BotocoreAPI
 from faas_profiler_python.payload import Payload
-
-
-class InvocationSpan:
-    """
-    Represents a lambda invocation
-    """
-
-    _logger = logging.getLogger("DistributedTracer")
-    _logger.setLevel(logging.INFO)
-
-    @classmethod
-    def create_from_incoming_payload(
-        cls,
-        payload: Type[Payload]
-    ) -> Type[InvocationSpan]:
-        parent_ctx = payload.extract_tracing_context()
-
-        if parent_ctx.profile_id is None:
-            cls._logger.info(
-                "No profile id found. Creating Span with new profile id")
-
-        if parent_ctx.root_id is None:
-            cls._logger.info("No root id found. Treating Span as root span.")
-
-        return cls(
-            profile_id=parent_ctx.profile_id,
-            root_id=parent_ctx.span_id)
-
-    def __init__(
-        self,
-        profile_id: str = None,
-        root_id: str = None,
-        span_id: str = None
-    ) -> None:
-        self.profile_id = profile_id if profile_id else uuid4()
-        self.root_id = root_id
-        self.span_id = span_id if span_id else uuid4()
-
-        self._logger.info(f"NEW SPAN: {self}")
-
-    def __str__(self) -> str:
-        return f"[profile_id={self.profile_id}, root_id={self.root_id}, span_id={self.span_id}]"
-
-    @property
-    def is_root(self) -> bool:
-        return self.root_id is None
-
-    @property
-    def inject_context(self) -> dict:
-        """
-        Returns the span as injectable context.
-        """
-        ctx = {}
-        if self.profile_id:
-            ctx[PROFILE_ID_HEADER] = str(self.profile_id)
-        if self.root_id:
-            ctx[ROOT_ID_HEADER] = str(self.root_id)
-        if self.span_id:
-            ctx[SPAN_ID_HEADER] = str(self.span_id)
-
-        return ctx
 
 
 class DistributedTracer:
@@ -92,40 +24,102 @@ class DistributedTracer:
     _logger = logging.getLogger("DistributedTracer")
     _logger.setLevel(logging.INFO)
 
-    # @classmethod
-    # def find_tracer(cls, provider: Provider) -> DistributedTracer:
-    #     if provider == Provider.AWS:
-    #         return AWSDistributedTracer()
-    #     else:
-    #         cls._logger.warn(
-    #             "Could not find cloud provider specific tracer. Take base tracer.")
-    #         return cls()
+    outbound_libraries = [
+        BotocoreAPI
+    ]
 
     def __init__(
         self,
-        config: Type[ProfileConfig],
-        provider: Provider,
-        context: Type[ProfileContext]
+        payload: Type[Payload]
     ) -> None:
-        self.config = config
-        self.provider = provider
-        self.context = context
-        self.payload = None
-
-        self.current_span: Type[InvocationSpan] = None
-
-        self.aws_injection = AWSInjection()
-
-    @contextmanager
-    def start(self, payload: Type[Payload]):
         self.payload = payload
-        self.current_span = InvocationSpan.create_from_incoming_payload(
+        self.current_invocation_span = InvocationSpan.create_from_incoming_payload(
             self.payload)
+        self.outbound_patchers = self._patch_outbound_libraries()
 
-        self.inject_request()
+    @property
+    def context(self) -> Type[TraceContext]:
+        """
+        Returns the current trace context given by the invocation span.
+        """
+        return self.current_invocation_span.trace_context
 
-        yield
+    def _patch_outbound_libraries(self):
+        """
+        Patches all outbound libraries
+        """
+        outbound_patchers = {}
+        for outbound_library in self.outbound_libraries:
+            patcher = request_patcher(outbound_library)
+            patcher.set_tracer(self)
+            patcher.activate()
 
-    def inject_request(self):
-        self.aws_injection.inject_api_calls(
-            data_to_inject=self.current_span.inject_context)
+            outbound_patchers[outbound_library] = patcher
+
+        return outbound_patchers
+
+
+class InvocationSpan:
+    """
+    Represents a lambda invocation
+    """
+
+    _logger = logging.getLogger("InvocationSpan")
+    _logger.setLevel(logging.INFO)
+
+    @classmethod
+    def create_from_incoming_payload(
+        cls,
+        payload: Type[Payload]
+    ) -> Type[InvocationSpan]:
+        parent_ctx = payload.extract_tracing_context()
+
+        trace_id = None
+        if parent_ctx.trace_id:
+            trace_id = parent_ctx.trace_id
+        else:
+            cls._logger.info(
+                "No trace id found. Creating Span with new trace id")
+
+        parent_id = None
+        if parent_ctx.invocation_id:
+            parent_id = parent_ctx.invocation_id
+        else:
+            cls._logger.info(
+                "No invocation id found. Treating Span as root span.")
+
+        return cls(
+            trace_id=trace_id,
+            parent_id=parent_id)
+
+    def __init__(
+        self,
+        trace_id: str = None,
+        parent_id: str = None,
+    ) -> None:
+        self.trace_id = trace_id if trace_id else uuid4()
+        self.invocation_id = uuid4()
+        self.parent_id = parent_id
+
+        self._trace_context = TraceContext(
+            self.trace_id, self.invocation_id, self.parent_id)
+
+        self._logger.info(f"NEW SPAN: {self}")
+
+    def __str__(self) -> str:
+        return f"[trace_id={self.trace_id}, invocation_id={self.invocation_id}, parent_id={self.parent_id}]"
+
+    @property
+    def is_root(self) -> bool:
+        return self.parent_id is None
+
+    @property
+    def trace_context(self) -> Type[TraceContext]:
+        """
+        Returns the trace context of this span
+        """
+        return self._trace_context
+
+    @property
+    def trigger_context(self) -> Any:
+        return None
