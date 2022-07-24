4d26a3eccf5d7baad12e599e088c00896636d47d
Malte Wächter <git@malte-waechter.com>
2022-07-24 22:59:04 +0200

    Update capture format

--- faas_profiler_python/captures/__init__.py
+++ faas_profiler_python/captures/__init__.py
@@ -1,7 +1,57 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Package for invocation capturing.
+Module for invocation capturing.
 """
 
-from .aws import * # noqa
+from __future__ import annotations
+
+import inspect
+
+from typing import Any, Type
+
+from faas_profiler_python.core import BasePlugin
+from faas_profiler_python.patchers import FunctionPatcher, PatchInvocation, request_patcher
+from faas_profiler_python.utilis import Loggable
+
+
+class Capture(BasePlugin, Loggable):
+    requested_patch: FunctionPatcher = None
+
+    def __new__(cls: type[Capture]) -> Capture:
+        if cls.requested_patch is None:
+            raise ValueError(
+                f"Cannot initialize capture {cls} without requested patch.")
+
+        if not inspect.isclass(
+                cls.requested_patch) or not issubclass(
+                cls.requested_patch,
+                FunctionPatcher):
+            raise ValueError(
+                f"'requested_patch' needs to be class and a subclass of FunctionPatcher, got {cls.requested_patch}")
+
+        return BasePlugin.__new__(cls)
+
+    def initialize(self, parameters: dict = {}) -> None:
+        self.patcher = request_patcher(self.requested_patch)
+        self.patcher.register_capture(self)
+
+    def start(self) -> None:
+        self.patcher.activate()
+
+    def capture(
+        self,
+        patch_invocation: Type[PatchInvocation],
+        before_result: Any = None,
+        after_result: Any = None
+    ) -> None:
+        pass
+
+    def stop(self) -> None:
+        self.patcher.deactivate()
+
+    def deinitialize(self) -> None:
+        del self.patcher
+
+    def results(self) -> list:
+        return []
--- faas_profiler_python/captures/aws.py
+++ faas_profiler_python/captures/aws.py
@@ -6,31 +6,23 @@ Module for AWS invocation capturing.
 
 from typing import Type
 
-from faas_profiler_python.patchers import patch_module
-from faas_profiler_python.patchers.base import PatchEvent
-from faas_profiler_python.patchers.botocore import AWSApiCall, AWSApiResponse
-from faas_profiler_python.patchers.io import IOCall, IOReturn
-
-from faas_profiler_python.captures.base import Capture, register_with_name
+from faas_profiler_python.captures import Capture, PatchInvocation
+from faas_profiler_python.patchers.botocore import AWSApiCall, AWSApiResponse, BotocoreAPI
+from faas_profiler_python.patchers.io import Open, IOCall, IOReturn
 
 
-@register_with_name("AWS::S3Capture")
 class S3Capture(Capture):
+    requested_patch = BotocoreAPI
 
-    def __init__(self, config: dict = {}) -> None:
-        self.patcher = patch_module("botocore")
-        self._s3_captures = []
+    def initialize(self, parameters: dict = {}) -> None:
+        self.invocations = []
+        return super().initialize(parameters)
 
-    def start(self):
-        self.patcher.start()
-        self.patcher.register_observer(
-            self, on=("botocore.client", "BaseClient._make_api_call"))
-
-    def __call__(
+    def capture(
         self,
-        patch_event: Type[PatchEvent],
-        aws_api_call: Type[AWSApiCall],
-        aws_api_response: Type[AWSApiResponse]
+        patch_invocation: Type[PatchInvocation],
+        aws_api_call: Type[AWSApiCall] = None,
+        aws_api_response: Type[AWSApiResponse] = None
     ) -> None:
         if aws_api_response is None or aws_api_response is None:
             return
@@ -38,7 +30,7 @@ class S3Capture(Capture):
         if aws_api_call.service != "s3":
             return
 
-        self._s3_captures.append({
+        self.invocations.append({
             "operation": aws_api_call.operation,
             "bucket": aws_api_call.api_params.get("Bucket"),
             "key": aws_api_call.api_params.get("Key"),
@@ -48,57 +40,44 @@ class S3Capture(Capture):
             "request_method": aws_api_call.http_method,
             "request_status": aws_api_response.http_code,
             "size": aws_api_response.content_length,
-            "execution_time": patch_event.execution_time
+            "execution_time": patch_invocation.execution_time
         })
 
-    def stop(self) -> None:
-        self.patcher.deregister_observer(self)
-        self.patcher.stop()
-
-    def invocations(self) -> list:
-        return self._s3_captures
+    def results(self) -> list:
+        return self.invocations
 
 
-@register_with_name("AWS::EFSCapture")
 class EFSCapture(Capture):
+    requested_patch = Open
 
-    def __init__(self, config: dict = {}) -> None:
-        self.mounting_points = config.get("mount_points")
+    def initialize(self, parameters: dict = {}) -> None:
+        self.mounting_points = parameters.get("mount_points")
+        self.invocations = []
 
         if not self.mounting_points:
             raise ValueError(
                 "Cannot initialise EFSCapture without mounting points")
+        return super().initialize(parameters)
 
-        self.patcher = patch_module("io")
-        self._efs_captures = []
-
-    def start(self):
-        self.patcher.start()
-        self.patcher.register_observer(self, on=("builtins", "open"))
-
-    def __call__(
+    def capture(
         self,
-        patch_event: Type[PatchEvent],
-        io_call: Type[IOCall],
-        io_return: Type[IOReturn]
+        patch_invocation: Type[PatchInvocation],
+        io_call: Type[IOCall] = None,
+        io_return: Type[IOReturn] = None
     ) -> None:
         for monting_point in self.mounting_points:
             if io_call.file.startswith(monting_point):
-                self._efs_captures.append({
+                self.invocations.append({
                     "efs_mount": monting_point,
                     "file": io_call.file,
                     "mode": self._determine_mode(io_call.mode),
                     "encoding": io_return.encoding,
                     "io_type": str(io_return.wrapper_type),
-                    "execution_time": patch_event.execution_time
+                    "execution_time": patch_invocation.execution_time
                 })
 
-    def stop(self) -> None:
-        self.patcher.deregister_observer(self)
-        self.patcher.stop()
-
-    def invocations(self) -> list:
-        return self._efs_captures
+    def results(self) -> list:
+        return self.invocations
 
     def _determine_mode(self, mode: str) -> str:
         if "r+" in mode or "w+" in mode or "a" in mode:
