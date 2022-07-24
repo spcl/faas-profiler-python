7cf98df821bb506b45d0d59de7adab03f3b7b16a
Malte Wächter <git@malte-waechter.com>
2022-07-24 21:39:47 +0200

    Add init for measurements with base definitions

--- faas_profiler_python/measurements/__init__.py
+++ faas_profiler_python/measurements/__init__.py
@@ -1,13 +1,55 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Module of all measurements for python client.
+Base for Measurements.
+
+Defines abstract base class for all measurements and measuring points
 """
 
-from .base import * # noqa
-from .common import * # noqa
-from .network import * # noqa
-from .memory import * # noqa
-from .cpu import * # noqa
-from .information import * # noqa
-from .disk import * # noqa
+from __future__ import annotations
+from typing import Type
+
+
+from faas_profiler_python.config import ProfileContext
+from faas_profiler_python.core import BasePlugin
+from faas_profiler_python.utilis import Loggable
+
+
+class MeasurementError(RuntimeError):
+    pass
+
+
+class Measurement(BasePlugin, Loggable):
+    """
+    Base class for all measurements in FaaS Profiler.
+
+    Cannot be initialised.
+    """
+
+    def initialize(
+        self,
+        profile_context: Type[ProfileContext],
+        parameters: dict = {}
+    ) -> None:
+        pass
+
+    def start(self) -> None:
+        pass
+
+    def stop(self) -> None:
+        pass
+
+    def deinitialize(self) -> None:
+        pass
+
+    def results(self) -> dict:
+        pass
+
+
+class PeriodicMeasurement(Measurement):
+    """
+    Base class for measurements that are executed in parallel in another process.
+    """
+
+    def measure(self):
+        pass
