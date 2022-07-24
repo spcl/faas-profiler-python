a41b171f2117c47ab0f98c508d96deccf77fbf11
Malte Wächter <git@malte-waechter.com>
2022-07-25 01:30:02 +0200

    Update config file with new names

--- faas_profiler_python/config.py
+++ faas_profiler_python/config.py
@@ -287,18 +287,18 @@ Distrubted Tracing Config
 """
 
 # https://specs.openstack.org/openstack/api-wg/guidelines/headers.html
-PROFILE_ID_HEADER = "FaaS-Profiler-Profile-ID"
-ROOT_ID_HEADER = "FaaS-Profiler-Root-ID"
-SPAN_ID_HEADER = "FaaS-Profiler-Span-ID"
+TRACE_ID_HEADER = "FaaS-Profiler-Trace-ID"
+INVOCATION_ID_HEADER = "FaaS-Profiler-Invocation-ID"
+PARENT_ID_HEADER = "FaaS-Profiler-Parent-ID"
 
 TRACE_CONTEXT_KEY = "_faas_profiler_context"
 
 
 @dataclass
 class TraceContext:
-    profile_id: str = None
-    root_id: str = None
-    span_id: str = None
+    trace_id: str = None
+    invocation_id: str = None
+    parent_id: str = None
 
     @property
     def is_complete(self) -> bool:
@@ -306,6 +306,20 @@ class TraceContext:
         Returns True if trace context is complete
         """
         return (
-            self.profile_id is not None and
-            self.root_id is not None and
-            self.span_id is not None)
+            self.trace_id is not None and
+            self.invocation_id is not None and
+            self.parent_id is not None)
+
+    def to_injectable(self) -> dict:
+        """
+        Returns the context as injectable context.
+        """
+        ctx = {}
+        if self.trace_id:
+            ctx[TRACE_ID_HEADER] = str(self.trace_id)
+        if self.invocation_id:
+            ctx[INVOCATION_ID_HEADER] = str(self.invocation_id)
+        if self.parent_id:
+            ctx[PARENT_ID_HEADER] = str(self.parent_id)
+
+        return ctx
