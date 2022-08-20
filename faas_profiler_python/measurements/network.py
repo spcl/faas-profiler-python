650ad66d3bd06d0de7bc7334757426af5d1edc7d
Malte Wächter <git@malte-waechter.com>
2022-08-20 23:22:18 +0200

    Include new datastructures for network

--- faas_profiler_python/measurements/network.py
+++ faas_profiler_python/measurements/network.py
@@ -8,10 +8,14 @@ Module for network measurements:
 
 import psutil
 
-from typing import List, Set, Type
-from functools import reduce
+from typing import Set, Type
 
-from faas_profiler_python.measurements import PeriodicMeasurement
+from faas_profiler_core.models import (
+    NetworkConnectionItem,
+    NetworkConnections,
+    NetworkIOCounters
+)
+from faas_profiler_python.measurements import Measurement, PeriodicMeasurement
 
 
 class Connections(PeriodicMeasurement):
@@ -22,112 +26,76 @@ class Connections(PeriodicMeasurement):
         **kwargs
     ) -> None:
         self.process = None
-        self.connections: List[psutil._common.pconn] = []
-        self.socket_descriptors: Set[int] = set()
 
-        self._results = {}
+        self._result = NetworkConnections(
+            connections=[])
+        self._socket_descriptors: Set[int] = set()
 
         try:
             self.process = psutil.Process(function_pid)
         except psutil.Error as err:
             self._logger.warn(f"Could not set process: {err}")
 
-    def start(self):
-        self._update_connection()
-
     def measure(self):
         self._update_connection()
 
     def deinitialize(self) -> None:
-
-        self._results = {"connections": [{
-            "socket_descriptor": conn.fd,
-            "family": str(conn.family),
-            "local_address": f"{conn.laddr.ip}:{conn.laddr.port}",
-            "remote_address": f"{conn.raddr.ip}:{conn.raddr.port}",
-        } for conn in self.connections]}
-
         del self.process
-        del self.connections
-        del self.socket_descriptors
+
+    def results(self) -> dict:
+        return self._result.dump()
 
     def _update_connection(self):
         if self.process is None:
             return
 
         for conn in self.process.connections():
-            if conn.fd in self.socket_descriptors:
+            if conn.fd in self._socket_descriptors:
                 return
 
-            self.connections.append(conn)
-            self.socket_descriptors.add(conn.fd)
+            self._socket_descriptors.add(conn.fd)
+            self._result.connections.append(NetworkConnectionItem(
+                socket_descriptor=int(conn.fd),
+                socket_family=conn.family,
+                local_address=f"{conn.laddr.ip}:{conn.laddr.port}",
+                remote_address=f"{conn.raddr.ip}:{conn.raddr.port}"))
 
-    def results(self) -> dict:
-        return self._results
 
-
-class IOCounters(PeriodicMeasurement):
+class IOCounters(Measurement):
 
     def initialize(
         self,
-        per_interface: bool = False,
         **kwargs
     ) -> None:
-        self.per_interface = per_interface
-
-        self.start_snapshot: Type[psutil.snetio] = None
-        self.end_snapshot: Type[psutil.snetio] = None
+        self._start_snapshot: Type[psutil.snetio] = None
+        self._end_snapshot: Type[psutil.snetio] = None
 
         self._interfaces_counters = []
         self._total_counters = {}
 
     def start(self) -> None:
-        self.start_snapshot = psutil.net_io_counters(pernic=self.per_interface)
+        self._start_snapshot = psutil.net_io_counters(pernic=False)
 
     def stop(self) -> None:
-        self.end_snapshot = psutil.net_io_counters(pernic=self.per_interface)
-
-    def tearDown(self) -> None:
-
-        self._interfaces_counters = []
-        self._total_counters = {}
-
-        if self.per_interface:
-            for ifc, end_counters in self.end_snapshot.items():
-                start_counters = self.start_snapshot.get(ifc)
-                if start_counters:
-                    self._interfaces_counters.append({
-                        "interface": ifc,
-                        **_get_snapshot_net_io_delta(start_counters, end_counters)._asdict()
-                    })
-
-            self._total_counters = reduce(lambda a, b: {
-                k: v + a.get(k, 0) for k, v in b.items() if k != "interface"
-            }, self._interfaces_counters)
-        else:
-            self._total_counters = _get_snapshot_net_io_delta(
-                self.start_snapshot, self.end_snapshot)._asdict()
-
-        del self.start_snapshot
-        del self.end_snapshot
+        self._end_snapshot = psutil.net_io_counters(pernic=False)
 
     def results(self) -> dict:
-        return {
-            "interfaces": self._interfaces_counters,
-            "total": self._total_counters
-        }
+        return get_snapshot_io_delta(
+            self._start_snapshot,
+            self._end_snapshot
+        ).dump()
 
 
-def _get_snapshot_net_io_delta(
+def get_snapshot_io_delta(
     start_snapshot: Type[psutil._common.snetio],
     end_snapshot: Type[psutil._common.snetio]
-) -> Type[psutil._common.snetio]:
-    return psutil._common.snetio(
-        bytes_sent=end_snapshot.bytes_sent - start_snapshot.bytes_sent,
-        bytes_recv=end_snapshot.bytes_recv - start_snapshot.bytes_recv,
-        packets_sent=end_snapshot.packets_sent - start_snapshot.packets_sent,
-        packets_recv=end_snapshot.packets_recv - start_snapshot.packets_recv,
-        errin=end_snapshot.errin - start_snapshot.errin,
-        errout=end_snapshot.errout - start_snapshot.errout,
-        dropin=end_snapshot.dropin - start_snapshot.dropin,
-        dropout=end_snapshot.dropout - start_snapshot.dropout)
+) -> Type[NetworkIOCounters]:
+    return NetworkIOCounters(
+        bytes_sent=int(end_snapshot.bytes_sent - start_snapshot.bytes_sent),
+        bytes_received=int(end_snapshot.bytes_recv - start_snapshot.bytes_recv),
+        packets_sent=int(end_snapshot.packets_sent - start_snapshot.packets_sent),
+        packets_received=int(end_snapshot.packets_recv - start_snapshot.packets_recv),
+        error_in=int(end_snapshot.errin - start_snapshot.errin),
+        error_out=int(end_snapshot.errout - start_snapshot.errout),
+        drop_in=int(end_snapshot.dropin - start_snapshot.dropin),
+        drop_out=int(end_snapshot.dropout - start_snapshot.dropout))
