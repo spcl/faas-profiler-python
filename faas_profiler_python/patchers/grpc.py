6210b8eb5414b1f764a8d5fdac1d0e0a5a20a912
Malte Wächter <git@malte-waechter.com>
2022-09-10 15:48:30 +0200

    Add grpc patcher draft

--- faas_profiler_python/patchers/grpc.py
+++ faas_profiler_python/patchers/grpc.py
@@ -7,6 +7,7 @@ FIXME: Quick and dirty solution, adapt this with the framework
 """
 
 from __future__ import annotations
+import grpc
 
 import sys
 import logging
@@ -15,13 +16,6 @@ import multiprocessing
 from wrapt import wrap_function_wrapper
 from typing import Any, Callable, Type
 
-from faas_profiler_core.constants import AWSService, AWSOperation, Provider, TriggerSynchronicity
-from faas_profiler_core.models import OutboundContext, TracingContext
-from faas_profiler_python.config import InjectionError, UnsupportedServiceError
-
-from faas_profiler_python.patchers import FunctionPatcher, PatchContext
-from faas_profiler_python.utilis import get_arg_by_key_or_pos
-
 from grpc_interceptor import ClientCallDetails, ClientInterceptor
 
 _lock = multiprocessing.Lock()
@@ -29,7 +23,6 @@ _lock = multiprocessing.Lock()
 _logger = logging.getLogger("[GRPC PATCH]")
 _logger.setLevel(logging.INFO)
 
-import grpc
 
 def patch_with_hook(
     module_name: str,
@@ -50,19 +43,26 @@ def setup_grpc_interceptor():
 
     # PATCHING CLIENT
     with _lock:
-        patch_with_hook("grpc", "insecure_channel", _add_channel_intercepter_client)
-        patch_with_hook("grpc", "secure_channel", _add_channel_intercepter_client)
+        patch_with_hook(
+            "grpc",
+            "insecure_channel",
+            _add_channel_intercepter_client)
+        patch_with_hook(
+            "grpc",
+            "secure_channel",
+            _add_channel_intercepter_client)
         # patch_with_hook("grpc", "intercept_channel")
 
     # PATCHING SERVER
-    with _lock:
-        patch_with_hook("grpc", "server", _add_server_intercepter)
+    # with _lock:
+    #     patch_with_hook("grpc", "server", _add_server_intercepter)
+
 
 def remove_interceptor():
     pass
 
 
-### Channel patches
+# Channel patches
 
 def _add_channel_intercepter_client(
     channel_function: Type[Callable],
@@ -71,7 +71,7 @@ def _add_channel_intercepter_client(
     function_kwargs: dict,
 ):
     _grpc_channel = channel_function(*function_args, **function_kwargs)
-    return grpc.intercept_channel(_grpc_channel, Foo())
+    return grpc.intercept_channel(_grpc_channel, ClientInterceptor())
 
 
 def _add_server_intercepter(
@@ -82,48 +82,16 @@ def _add_server_intercepter(
 ):
     pass
 
-### Interceptors
+# Interceptors
 
-class Foo(ClientInterceptor):
+
+class ClientInterceptor(ClientInterceptor):
     def intercept(
         self,
         method: Callable,
         request_or_iterator: Any,
         call_details: Any,
     ):
-        """Override this method to implement a custom interceptor.
-
-        This method is called for all unary and streaming RPCs. The interceptor
-        implementation should call `method` using a `grpc.ClientCallDetails` and the
-        `request_or_iterator` object as parameters. The `request_or_iterator`
-        parameter may be type checked to determine if this is a singluar request
-        for unary RPCs or an iterator for client-streaming or client-server streaming
-        RPCs.
-
-        Args:
-            method: A function that proceeds with the invocation by executing the next
-                interceptor in the chain or invoking the actual RPC on the underlying
-                channel.
-            request_or_iterator: RPC request message or iterator of request messages
-                for streaming requests.
-            call_details: Describes an RPC to be invoked.
-
-        Returns:
-            The type of the return should match the type of the return value received
-            by calling `method`. This is an object that is both a
-            `Call <https://grpc.github.io/grpc/python/grpc.html#grpc.Call>`_ for the
-            RPC and a `Future <https://grpc.github.io/grpc/python/grpc.html#grpc.Future>`_.
-
-            The actual result from the RPC can be got by calling `.result()` on the
-            value returned from `method`.
-        """
-        new_details = ClientCallDetails(
-            call_details.method,
-            call_details.timeout,
-            [("authorization", "Bearer mysecrettoken")],
-            call_details.credentials,
-            call_details.wait_for_ready,
-            call_details.compression,
-        )
+        # DO STUFF
 
         return method(request_or_iterator, call_details)
