651df31d04992c9388b28fb647fc71adfe85e0b7
Malte Wächter <git@malte-waechter.com>
2022-09-12 23:32:55 +0200

    Remove google sdks

--- faas_profiler_python/exporters/common.py
+++ faas_profiler_python/exporters/common.py
@@ -4,13 +4,9 @@
 Common exporters
 """
 
-import boto3
-
 from typing import Type
 
-from faas_profiler_core.storage import S3RecordStorage, GCPRecordStorage
-
-from faas_profiler_python.exporters import Exporter, ResultCollector, json_formatter
+from faas_profiler_python.exporters import Exporter, ResultCollector
 
 
 class Console(Exporter):
@@ -25,71 +21,47 @@ class Console(Exporter):
         print(results_collector.raw_data)
 
 
-class Visualizer(Exporter):
-    """
-    Sends records to visualizer function
-    """
+# class AWSVisualizerUploader(Exporter):
+#     """
+#     Uploads record to visualizer bucket in S3.
+#     """
 
-    def __init__(self, function_name: str, region_name: str) -> None:
-        super().__init__()
+#     def __init__(
+#         self,
+#         bucket_name: str,
+#         region_name: str
+#     ) -> None:
+#         from faas_profiler_core.storage import S3RecordStorage
+#         self.record_storage = S3RecordStorage(
+#             bucket_name=bucket_name,
+#             region_name=region_name)
 
-        self.function_name = function_name
-        self.region_name = region_name
-        if self.function_name is None or self.region_name is None:
-            raise ValueError(
-                "Cannot initialize VisualizerExporter without function name or region name")
-
-        self.client = boto3.client("lambda", region_name=self.region_name)
-
-    def export(self, results_collector: Type[ResultCollector]):
-        """
-        Sends data to AWS Lambda function
-        """
-        self.client.invoke(
-            FunctionName=self.function_name,
-            InvocationType='Event',
-            Payload=results_collector.format(formatter=json_formatter))
+#     def export(self, results_collector: Type[ResultCollector]):
+#         """
+#         Uploads record as json to bucket.
+#         """
+#         self.record_storage.store_unprocessed_record(results_collector.record)
 
 
-class AWSVisualizerUploader(Exporter):
-    """
-    Uploads record to visualizer bucket in S3.
-    """
+# class GCPVisualizerUploader(Exporter):
+#     """
+#     Uploads record to visualizer bucket in Google Cloud Storage.
+#     """
 
-    def __init__(
-        self,
-        bucket_name: str,
-        region_name: str
-    ) -> None:
-        self.record_storage = S3RecordStorage(
-            bucket_name=bucket_name,
-            region_name=region_name)
+#     def __init__(
+#         self,
+#         project: str,
+#         bucket_name: str,
+#         region_name: str
+#     ) -> None:
+#         from faas_profiler_core.storage import GCPRecordStorage
+#         self.record_storage = GCPRecordStorage(
+#             project=project,
+#             bucket_name=bucket_name,
+#             region_name=region_name)
 
-    def export(self, results_collector: Type[ResultCollector]):
-        """
-        Uploads record as json to bucket.
-        """
-        self.record_storage.store_unprocessed_record(results_collector.record)
-
-
-class GCPVisualizerUploader(Exporter):
-    """
-    Uploads record to visualizer bucket in Google Cloud Storage.
-    """
-
-    def __init__(
-        self,
-        project: str,
-        bucket_name: str,
-        region_name: str
-    ) -> None:
-        self.record_storage = GCPRecordStorage(
-            project=project,
-            bucket_name=bucket_name,
-            region_name=region_name)
-
-    def export(self, results_collector: Type[ResultCollector]):
-        """
-        Uploads record as json to bucket.
-        """
-        self.record_storage.store_unprocessed_record(results_collector.record)
+#     def export(self, results_collector: Type[ResultCollector]):
+#         """
+#         Uploads record as json to bucket.
+#         """
+#         self.record_storage.store_unprocessed_record(results_collector.record)
