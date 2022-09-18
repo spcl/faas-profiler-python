e820a66b7d676a0d53e32281d6c9392862f166ab
Malte Wächter <git@malte-waechter.com>
2022-09-18 16:04:07 +0200

    Readd exporters

--- faas_profiler_python/exporters/common.py
+++ faas_profiler_python/exporters/common.py
@@ -21,47 +21,47 @@ class Console(Exporter):
         print(results_collector.raw_data)
 
 
-# class AWSVisualizerUploader(Exporter):
-#     """
-#     Uploads record to visualizer bucket in S3.
-#     """
+class AWSVisualizerUploader(Exporter):
+    """
+    Uploads record to visualizer bucket in S3.
+    """
 
-#     def __init__(
-#         self,
-#         bucket_name: str,
-#         region_name: str
-#     ) -> None:
-#         from faas_profiler_core.storage import S3RecordStorage
-#         self.record_storage = S3RecordStorage(
-#             bucket_name=bucket_name,
-#             region_name=region_name)
+    def __init__(
+        self,
+        bucket_name: str,
+        region_name: str
+    ) -> None:
+        from faas_profiler_core.storage import S3RecordStorage
+        self.record_storage = S3RecordStorage(
+            bucket_name=bucket_name,
+            region_name=region_name)
 
-#     def export(self, results_collector: Type[ResultCollector]):
-#         """
-#         Uploads record as json to bucket.
-#         """
-#         self.record_storage.store_unprocessed_record(results_collector.record)
+    def export(self, results_collector: Type[ResultCollector]):
+        """
+        Uploads record as json to bucket.
+        """
+        self.record_storage.store_unprocessed_record(results_collector.record)
 
 
-# class GCPVisualizerUploader(Exporter):
-#     """
-#     Uploads record to visualizer bucket in Google Cloud Storage.
-#     """
+class GCPVisualizerUploader(Exporter):
+    """
+    Uploads record to visualizer bucket in Google Cloud Storage.
+    """
 
-#     def __init__(
-#         self,
-#         project: str,
-#         bucket_name: str,
-#         region_name: str
-#     ) -> None:
-#         from faas_profiler_core.storage import GCPRecordStorage
-#         self.record_storage = GCPRecordStorage(
-#             project=project,
-#             bucket_name=bucket_name,
-#             region_name=region_name)
+    def __init__(
+        self,
+        project: str,
+        bucket_name: str,
+        region_name: str
+    ) -> None:
+        from faas_profiler_core.storage import GCPRecordStorage
+        self.record_storage = GCPRecordStorage(
+            project=project,
+            bucket_name=bucket_name,
+            region_name=region_name)
 
-#     def export(self, results_collector: Type[ResultCollector]):
-#         """
-#         Uploads record as json to bucket.
-#         """
-#         self.record_storage.store_unprocessed_record(results_collector.record)
+    def export(self, results_collector: Type[ResultCollector]):
+        """
+        Uploads record as json to bucket.
+        """
+        self.record_storage.store_unprocessed_record(results_collector.record)
