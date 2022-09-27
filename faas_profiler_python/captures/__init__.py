2e596933baa9d36f71b1f0e034ee68ca70029da8
Malte Wächter <git@malte-waechter.com>
2022-09-27 09:40:56 +0200

    Update capture for IO and AWS design

--- faas_profiler_python/captures/__init__.py
+++ faas_profiler_python/captures/__init__.py
@@ -6,34 +6,74 @@ Module for invocation capturing.
 
 from __future__ import annotations
 
-import inspect
+import logging
+import importlib
 
-from typing import Type, Any
+from typing import Type, Any, Tuple
 
 from faas_profiler_python.core import BasePlugin
-from faas_profiler_python.patchers import FunctionPatcher, OutboundContext, request_patcher
+from faas_profiler_python.patchers import OutboundContext
 from faas_profiler_python.utilis import Loggable
+from faas_profiler_python.config import LoadedPlugin
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+AVAILABLE_CAPTURES = {
+    "aws::S3Access": "faas_profiler_python.captures.aws.S3Access",
+    "aws::EFSAccess": "faas_profiler_python.captures.aws.EFSAccess"
+}
+
+
+def load_capture(name: str, parameters: dict = {}) -> LoadedPlugin:
+    """
+    Loads a single capture
+    """
+    if name not in AVAILABLE_CAPTURES:
+        raise RuntimeError(f"No capture with name {name} found")
+
+    module_str, klass_str = AVAILABLE_CAPTURES[name].rsplit(".", 1)
+
+    try:
+        module = importlib.import_module(module_str)
+        klass = getattr(module, klass_str)
+        return LoadedPlugin(name, klass, parameters)
+    except (ImportError, AttributeError):
+        raise RuntimeError(
+            f"No module found {module_str} with capture class {klass_str}")
+
+
+def load_all_captures(captures: list = []) -> Tuple[list, list]:
+    """
+    Loads all captures
+    """
+    loaded_captures = []
+    for capture in captures:
+        try:
+            loaded_plugin = load_capture(
+                name=capture.get("name"),
+                parameters=capture.get(
+                    "parameters",
+                    {}))
+            loaded_captures.append(loaded_plugin)
+        except Exception as err:
+            logger.error(f"Failed to load capture plugin: {err}")
+
+    return loaded_captures
 
 
 class Capture(BasePlugin, Loggable):
-    requested_patch: FunctionPatcher = None
+    requested_patch: str = None
 
     def __new__(cls: type[Capture]) -> Capture:
         if cls.requested_patch is None:
             raise ValueError(
                 f"Cannot initialize capture {cls} without requested patch.")
 
-        if not inspect.isclass(
-                cls.requested_patch) or not issubclass(
-                cls.requested_patch,
-                FunctionPatcher):
-            raise ValueError(
-                f"'requested_patch' needs to be class and a subclass of FunctionPatcher, got {cls.requested_patch}")
-
         return BasePlugin.__new__(cls)
 
-    def initialize(self, *args, **kwargs) -> None:
-        self.patcher = request_patcher(self.requested_patch)
+    def initialize(self, profiler, *args, **kwargs) -> None:
+        self.patcher = profiler.register_patcher(self.requested_patch)
         self.patcher.register_observer(self.capture)
 
     def start(self) -> None:
--- faas_profiler_python/captures/aws.py
+++ faas_profiler_python/captures/aws.py
@@ -8,26 +8,22 @@ from typing import Type
 
 from faas_profiler_python.captures import Capture
 from faas_profiler_python.patchers import OutboundContext
-from faas_profiler_python.patchers.botocore import BotocoreAPI
-from faas_profiler_python.patchers.io import OpenIO
 
 from faas_profiler_core.constants import (
     Provider,
     InternalService,
-    InternalOperation,
-    AWSService,
-    AWSOperation
+    AWSService
 )
 from faas_profiler_core.models import (
-    S3Capture,
+    S3Accesses,
     S3AccessItem,
-    EFSAccessItem,
-    EFSCapture
+    EFSAccesses,
+    EFSAccessItem
 )
 
 
 class S3Access(Capture):
-    requested_patch = BotocoreAPI
+    requested_patch = "botocore"
 
     def initialize(
         self,
@@ -37,15 +33,9 @@ class S3Access(Capture):
     ) -> None:
         super().initialize(*args, **kwargs)
 
-        self._bucket_names = bucket_names
-        self._captured_buckets = set()
+        self.bucket_names = bucket_names
+        self._result = S3Accesses(accesses=[])
 
-        self._obj_created = {}
-        self._obj_deleted = {}
-        self._obj_get = {}
-        self._obj_head = {}
-
-    # flake8: noqa: C901
     def capture(
         self,
         outbound_context: Type[OutboundContext]
@@ -56,114 +46,41 @@ class S3Access(Capture):
         if outbound_context.service != AWSService.S3:
             return
 
-        _bucket_name = outbound_context.identifier.get("bucket_name")
-        _object_key = outbound_context.identifier.get("object_key")
-        _size = outbound_context.tags.get("size")
-        _execution_time = outbound_context.overhead_time
+        bucket_name = outbound_context.identifier.get("bucket_name")
+        object_key = outbound_context.identifier.get("object_key")
+        size = outbound_context.tags.get("size")
+        execution_time = outbound_context.overhead_time
 
-        if self._bucket_names and _bucket_name not in self._bucket_names:
+        if self.bucket_names and bucket_name not in self._bucket_names:
             self.logger.info(
-                f"[S3 Capture]: Ignore S3 Operation on bucket {_bucket_name} and"
+                f"[S3 Capture]: Ignore S3 Operation on bucket {bucket_name} and"
                 "object {_object_key}. Bucket is not of target.")
             return
 
-        self._captured_buckets.add(_bucket_name)
+        self._result.accesses.append(S3AccessItem(
+            mode=outbound_context.operation.value,
+            bucket_name=bucket_name,
+            object_key=object_key,
+            object_size=size,
+            execution_time=execution_time
+        ))
 
-        if outbound_context.operation == AWSOperation.S3_OBJECT_GET:
-            bkt_obj_get = self._obj_get.setdefault(_bucket_name, {})
-            prev_gets = bkt_obj_get.setdefault(_object_key, ([], []))
-
-            if _size:
-                prev_gets[0].append(_size)
-            if _execution_time:
-                prev_gets[1].append(_execution_time)
-
-        if outbound_context.operation == AWSOperation.S3_OBJECT_CREATE:
-            bkt_obj_create = self._obj_created.setdefault(_bucket_name, {})
-            prev_creates = bkt_obj_create.setdefault(_object_key, ([], []))
-
-            if _size:
-                prev_creates[0].append(_size)
-            if _execution_time:
-                prev_creates[1].append(_execution_time)
-
-        if outbound_context.operation == AWSOperation.S3_OBJECT_REMOVED:
-            bkt_obj_deleted = self._obj_deleted.setdefault(_bucket_name, {})
-            prev_deletes = bkt_obj_deleted.setdefault(_object_key, ([], []))
-
-            if _size:
-                prev_deletes[0].append(_size)
-            if _execution_time:
-                prev_deletes[1].append(_execution_time)
-
-        if outbound_context.operation == AWSOperation.S3_OBJECT_HEAD:
-            bkt_obj_head = self._obj_head.setdefault(_bucket_name, {})
-            prev_heads = bkt_obj_head.setdefault(_object_key, ([], []))
-
-            if _execution_time:
-                prev_heads[1].append(_execution_time)
-
-    def results(self) -> list:
-        captures = []
-
-        for bkt_name in self._captured_buckets:
-            captures.append(
-                S3Capture(
-                    bucket_name=bkt_name,
-                    get_objects=self._capture_mappings_to_items(
-                        self._obj_get.get(
-                            bkt_name,
-                            {}),
-                        mode=AWSOperation.S3_OBJECT_GET),
-                    create_objects=self._capture_mappings_to_items(
-                        self._obj_created.get(
-                            bkt_name,
-                            {}),
-                        mode=AWSOperation.S3_OBJECT_CREATE),
-                    deleted_objects=self._capture_mappings_to_items(
-                        self._obj_deleted.get(
-                            bkt_name,
-                            {}),
-                        mode=AWSOperation.S3_OBJECT_REMOVED),
-                    head_objects=self._capture_mappings_to_items(
-                        self._obj_head.get(
-                            bkt_name,
-                            {}),
-                        mode=AWSOperation.S3_OBJECT_HEAD)).dump())
-
-        return captures
-
-    def _capture_mappings_to_items(
-        self,
-        capture_mapping: dict,
-        mode: AWSOperation
-    ) -> list:
-        items = []
-        for obj_k, (sizes, exe_times) in capture_mapping.items():
-            items.append(S3AccessItem(
-                mode=mode,
-                object_key=obj_k,
-                object_sizes=sizes,
-                execution_times=exe_times))
-
-        return items
+    def results(self) -> dict:
+        return self._result.dump()
 
 
 class EFSAccess(Capture):
-    requested_patch = OpenIO
+    requested_patch = "open_io"
 
-    def initialize(self, mounting_points: list, *args, **kwargs) -> None:
+    def initialize(self, mount_point: str, *args, **kwargs) -> None:
         super().initialize(*args, **kwargs)
-        self._mounting_points = mounting_points
+        self.mount_point = mount_point
 
-        if not self._mounting_points:
+        if not self.mount_point:
             raise ValueError(
-                "Cannot initialise EFSCapture without mounting points")
+                "Cannot initialise EFSCapture without mount_point")
 
-        self._file_reads = {
-            mnt_point: {} for mnt_point in self._mounting_points}
-        self._file_writes = {
-            mnt_point: {} for mnt_point in self._mounting_points}
+        self._result = EFSAccesses(mount_point=mount_point, accesses=[])
 
     def capture(
         self,
@@ -173,55 +90,24 @@ class EFSAccess(Capture):
         Capture a EFS file access.
         """
         if outbound_context.provider != Provider.INTERNAL:
+            print("hoer")
             return
 
         if outbound_context.service != InternalService.IO:
+            print("da")
             return
 
-        _file = outbound_context.tags.get("file")
-        _size = outbound_context.tags.get("size")
-        _execution_time = outbound_context.overhead_time
+        file = outbound_context.tags.get("file")
+        if not str(file).startswith(self.mount_point):
+            print("dort")
+            return
 
-        for mounting_point in self._mounting_points:
-            _efs_mnt_reads = self._file_reads[mounting_point]
-            _efs_mnt_writes = self._file_writes[mounting_point]
-            if _file and str(_file).startswith(mounting_point):
-                if (outbound_context.operation == InternalOperation.IO_READ or
-                        outbound_context.operation == InternalOperation.IO_READ_WRITE):
-                    prev_reads = _efs_mnt_reads.setdefault(_file, ([], []))
-                    if _size is not None:
-                        prev_reads[0].append(_size)
-                    if _execution_time:
-                        prev_reads[1].append(_execution_time)
+        self._result.accesses.append(EFSAccessItem(
+            mode=outbound_context.operation.value,
+            file=file,
+            file_size=outbound_context.tags.get("size"),
+            execution_time=outbound_context.overhead_time
+        ))
 
-                if (outbound_context.operation == InternalOperation.IO_WRITE or
-                        outbound_context.operation == InternalOperation.IO_READ_WRITE):
-                    prev_writes = _efs_mnt_writes.setdefault(_file, ([], []))
-                    if _size is not None:
-                        prev_writes[0].append(_size)
-                    if _execution_time:
-                        prev_writes[1].append(_execution_time)
-
-    def results(self) -> list:
-        captures = []
-        for mounting_point in self._mounting_points:
-            capture = EFSCapture(mounting_point)
-            mnt_writes = self._file_writes.get(mounting_point, {})
-            for file, (sizes, execution_times) in mnt_writes.items():
-                capture.written_files.append(EFSAccessItem(
-                    mode=InternalOperation.IO_WRITE,
-                    file=file,
-                    file_sizes=sizes,
-                    execution_times=execution_times))
-
-            mnt_reads = self._file_reads.get(mounting_point, {})
-            for file, (sizes, execution_times) in mnt_reads.items():
-                capture.read_files.append(EFSAccessItem(
-                    mode=InternalOperation.IO_READ,
-                    file=file,
-                    file_sizes=sizes,
-                    execution_times=execution_times))
-
-            captures.append(capture.dump())
-
-        return captures
+    def results(self) -> dict:
+        return self._result.dump()
