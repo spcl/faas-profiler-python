d09fa2a747b24fc6f8aea084d3cbd03b0b9f8065
Malte Wächter <git@malte-waechter.com>
2022-09-27 09:47:47 +0200

    Allow send data via pipe

--- faas_profiler_python/core.py
+++ faas_profiler_python/core.py
@@ -8,40 +8,18 @@ from __future__ import annotations
 import json
 import sys
 import os
-import importlib
-import importlib.util
-import inspect
 import logging
 import traceback
 
 from typing import List, Type
 from multiprocessing import Process, connection
 from abc import ABC
+from uuid import uuid4
 
 from faas_profiler_core.constants import RecordDataType
 
-from faas_profiler_python.config import (
-    MeasuringState,
-    ProcessFeedback,
-    LoadedPlugin,
-    UnresolvedPlugin
-)
-from faas_profiler_python.utilis import Loggable, split_plugin_name
-
-
-def split_plugin_list_by_subclass(plugins: list, subclass: Type) -> tuple:
-    """
-    Splits a list of classes into two groups based on a parent class.
-    """
-    subcls_group = []
-    remainder = []
-    for plugin in plugins:
-        if issubclass(plugin.cls, subclass):
-            subcls_group.append(plugin)
-        else:
-            remainder.append(plugin)
-
-    return (subcls_group, remainder)
+from faas_profiler_python.config import MeasuringState, LoadedPlugin
+from faas_profiler_python.utilis import Loggable
 
 
 class BasePlugin(ABC):
@@ -52,53 +30,6 @@ class BasePlugin(ABC):
 
     _logger = logging.getLogger("BasePlugin")
     _logger.setLevel(logging.INFO)
-
-    @classmethod
-    def load(
-        cls,
-        requested_plugins: List[UnresolvedPlugin]
-    ) -> List[BasePlugin]:
-        """
-        Loads a list of unresolved plugins found in the configuration.
-        """
-        loaded_plugins = []
-        for requested_plugin in requested_plugins:
-            _name = requested_plugin.name
-            _modules, _klass = split_plugin_name(_name)
-            _sub_module_name = ".".join(_modules)
-
-            try:
-                if requested_plugin.external_path:
-                    _full_module_spec = requested_plugin.external_path
-                    spec = importlib.util.spec_from_file_location(
-                        _sub_module_name, requested_plugin.external_path)
-                    imported_module = importlib.util.module_from_spec(spec)
-                    sys.modules[_sub_module_name] = imported_module
-                    spec.loader.exec_module(imported_module)
-                else:
-                    _full_module_spec = f"{cls.__module__}.{_sub_module_name}"
-                    imported_module = importlib.import_module(
-                        _full_module_spec)
-            except (ImportError, OSError, ModuleNotFoundError) as err:
-                cls._logger.error(
-                    f"Could not import {_full_module_spec} for plugin {_name}: {err}")
-                continue
-
-            if hasattr(imported_module, _klass):
-                _plugin = getattr(imported_module, _klass)
-                if inspect.isclass(_plugin) and issubclass(_plugin, cls):
-                    loaded_plugins.append(LoadedPlugin(
-                        _name, _plugin, requested_plugin.parameters))
-                    cls._logger.info(
-                        f"Loaded plugin for {_name} at {_full_module_spec}")
-                else:
-                    cls._logger.error(
-                        f"Requested attribute {_plugin} is not a class or not a subclass of {cls}")
-            else:
-                cls._logger.error(
-                    f"In module {_full_module_spec} no attribute with {_klass} was found.")
-
-        return loaded_plugins
 
 
 class BatchExecution(Loggable):
@@ -116,6 +47,13 @@ class BatchExecution(Loggable):
         self.plugin_objs = {}
 
         self.batch_type = batch_type
+
+    @property
+    def has_plugins(self) -> bool:
+        """
+        Returns True if batch has plugins
+        """
+        return len(self.plugins) > 0
 
     def initialize(self, *args, **kwargs):
         """
@@ -164,18 +102,18 @@ class BatchExecution(Loggable):
                 self.logger.error(
                     f"Measuring {plugin_obj} failed: {err}. Traceback: {traceback.format_exc()}")
 
-    def export_results(self) -> list:
+    def export_results(self) -> dict:
         """
         Returns a list of results
         """
-        results = []
+        results = {}
         for name, plugin_obj in self.plugin_objs.items():
             try:
-                results.append({
+                results[name] = {
                     "name": name,
                     "type": self.batch_type.name,
                     "results": plugin_obj.results()
-                })
+                }
             except Exception as err:
                 self.logger.error(
                     f"Exporting results for {plugin_obj} failed: {err}. Traceback: {traceback.format_exc()}")
@@ -199,24 +137,31 @@ class PeriodicProcess(Process):
     Process to run all measurements that are to be executed in parallel to the main process.
     """
 
+    PIPE_MAX_DATA = 33554432  # Bytes 32MiB = 3 * 2 ** 20 Bytes
+
     _logger = logging.getLogger("MeasurementProcess")
     _logger.setLevel(logging.INFO)
 
     def __init__(
         self,
-        batch: Type[BatchExecution],
+        periodic_measurements: List[LoadedPlugin],
         function_pid: int,
-        result_storage_path: str,
         parent_connection: Type[connection.Connection],
         child_connection: Type[connection.Connection],
         refresh_interval: float = 0.01
     ) -> None:
+        self.periodic_measurements = periodic_measurements
+        self.periodic_batch = BatchExecution(self.periodic_measurements)
+
         self.parent_connection = parent_connection
         self.child_connection = child_connection
         self.refresh_interval = refresh_interval
-        self.batch_execution = batch
+
         self.function_pid = function_pid
-        self.result_storage_path = result_storage_path
+        self.process_id = os.getpid()
+
+        self.result_storage_path = os.path.join(
+            "/", "tmp", f"{uuid4()}_pid_{self.process_id}.json")
 
         super(PeriodicProcess, self).__init__()
 
@@ -229,42 +174,49 @@ class PeriodicProcess(Process):
         Then stops all measurements and sends the results to the main process.
         """
         try:
-            process_pid = os.getpid()
-            self._logger.info(
-                f"Measurement process started (pid={process_pid}).")
-
-            self.batch_execution.initialize(
-                process_pid=process_pid,
+            # Initialize
+            self.periodic_batch.initialize(
+                process_pid=self.process_id,
                 function_pid=self.function_pid,
                 interval=self.refresh_interval)
-            self.batch_execution.start()
-            self.child_connection.send(ProcessFeedback(MeasuringState.STARTED))
 
-            self._logger.info("Measurement process started measuring.")
+            # Start
+            self.periodic_batch.start()
+            self.child_connection.send({"state": MeasuringState.STARTED})
 
+            # Measure
             state = MeasuringState.STARTED
             while state == MeasuringState.STARTED:
-                self.batch_execution.measure()
+                self.periodic_batch.measure()
 
                 if self.child_connection.poll(self.refresh_interval):
-                    state = self.child_connection.recv()
+                    state = self.child_connection.recv().get("state")
 
-            self._logger.info("Measurement process stopped measuring.")
+            # Stop
+            self.periodic_batch.stop()
+            self.child_connection.send({"state": MeasuringState.STOPPED})
 
-            self.batch_execution.stop()
-            self.child_connection.send(ProcessFeedback(MeasuringState.STOPPED))
+            # Export
+            results = self.periodic_batch.export_results()
+            if sys.getsizeof(results) < self.PIPE_MAX_DATA:
+                self.child_connection.send({
+                    "state": MeasuringState.EXPORT_DATA,
+                    "data": results})
+            else:
+                with open(self.result_storage_path, "w+") as fp:
+                    json.dump(results, fp)
 
-            results = self.batch_execution.export_results()
-            with open(self.result_storage_path, "w+") as fp:
-                json.dump(results, fp)
+                self.child_connection.send({
+                    "state": MeasuringState.EXPORT_DATA,
+                    "data": self.result_storage_path})
 
-            self.batch_execution.deinitialize()
-        except Exception as e:
-            tb = traceback.format_exc()
-            self.child_connection.send(ProcessFeedback(
-                state=MeasuringState.ERROR,
-                data=(e, tb)
-            ))
+            # Deinitialize
+            self.periodic_batch.deinitialize()
+        except Exception as error:
+            self.child_connection.send({
+                "state": MeasuringState.ERROR,
+                "data": (error, traceback.format_exc())
+            })
 
     def wait_for_state(self, state: MeasuringState, timeout: int = 10):
         """
@@ -274,10 +226,10 @@ class PeriodicProcess(Process):
         """
         if self.parent_connection.poll(timeout):
             feedback = self.parent_connection.recv()
-            if feedback.state == state:
+            if feedback.get("state") == state:
                 return True
-            elif feedback.state == MeasuringState.ERROR:
-                error, tb = feedback.data
+            elif feedback.get("state") == MeasuringState.ERROR:
+                error, tb = feedback.get("data")
                 print(tb)
                 raise error
 
