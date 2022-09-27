bb94a32aa21966cb14440aac8f85f39e9407e6d9
Malte Wächter <git@malte-waechter.com>
2022-09-27 09:42:36 +0200

    Optimize exporters

--- faas_profiler_python/exporters/__init__.py
+++ faas_profiler_python/exporters/__init__.py
@@ -5,19 +5,71 @@ Module for exporting and collecting results.
 """
 from __future__ import annotations
 
-import json
+import importlib
+import ujson
 import yaml
+import logging
+
+from typing import List
 
 from faas_profiler_python.core import BasePlugin
 from faas_profiler_python.utilis import Loggable
+from faas_profiler_python.config import LoadedPlugin
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+AVAILABLE_EXPORTERS = {
+    "common::Console": "faas_profiler_python.exporters.common.Console",
+    "common::AWSVisualizerUploader": "faas_profiler_python.exporters.common.AWSVisualizerUploader",
+    "common::GCPVisualizerUploader": "faas_profiler_python.exporters.common.GCPVisualizerUploader"
+}
+
+
+def load_exporter(name: str, parameters: dict = {}) -> LoadedPlugin:
+    """
+    Loads a single exporter
+    """
+    if name not in AVAILABLE_EXPORTERS:
+        raise RuntimeError(f"No exporter with name {name} found")
+
+    module_str, klass_str = AVAILABLE_EXPORTERS[name].rsplit(".", 1)
+
+    try:
+        module = importlib.import_module(module_str)
+        klass = getattr(module, klass_str)
+        return LoadedPlugin(name, klass, parameters)
+    except (ImportError, AttributeError):
+        raise RuntimeError(
+            f"No module found {module_str} with exporter class {klass_str}")
+
+
+def load_all_exporters(exporters: list = []) -> List[LoadedPlugin]:
+    """
+    Loads all exporters.
+    """
+    loaded_exporters = []
+    for exporter in exporters:
+        try:
+            loaded_plugin = load_exporter(
+                name=exporter.get("name"),
+                parameters=exporter.get(
+                    "parameters",
+                    {}))
+            loaded_exporters.append(loaded_plugin)
+        except Exception as err:
+            logger.error(f"Failed to load exporter plugin: {err}")
+
+    return loaded_exporters
 
 
 def json_formatter(raw_data: dict) -> str:
     def default(o): return f"<<non-serializable: {type(o).__qualname__}>>"
-    return json.dumps(
+    return ujson.dumps(
         raw_data,
         ensure_ascii=False,
-        indent=None,
+        indent=0,
         default=default
     ).encode('utf-8')
 
--- faas_profiler_python/exporters/common.py
+++ faas_profiler_python/exporters/common.py
@@ -4,7 +4,13 @@
 Common exporters
 """
 
-from faas_profiler_python.exporters import Exporter
+from uuid import uuid4
+from faas_profiler_python.exporters import Exporter, json_formatter
+import boto3
+
+UNPROCESSED_RECORDS_PREFIX = "unprocessed_records/"
+UNPROCESSED_RECORDS_FORMAT = UNPROCESSED_RECORDS_PREFIX + \
+    "{record_id}.json"
 
 
 class Console(Exporter):
@@ -24,42 +30,49 @@ class AWSVisualizerUploader(Exporter):
     Uploads record to visualizer bucket in S3.
     """
 
+    client = boto3.client('s3', region_name="eu-central-1")
+
     def __init__(
         self,
-        bucket_name: str,
-        region_name: str
+        bucket_name: str
     ) -> None:
-        from faas_profiler_core.storage import S3RecordStorage
-        self.record_storage = S3RecordStorage(
-            bucket_name=bucket_name,
-            region_name=region_name)
+        self.bucket_name = bucket_name
 
     def export(self, trace_record: dict) -> None:
         """
         Uploads record as json to bucket.
         """
-        self.record_storage.store_unprocessed_record(trace_record)
+        record_id = trace_record.get("tracing_context", {}).get(
+            "record_id", uuid4())
+        record_key = UNPROCESSED_RECORDS_FORMAT.format(
+            record_id=record_id)
+        record_json = json_formatter(trace_record)
+
+        self.client.put_object(
+            Bucket=self.bucket_name,
+            Key=record_key,
+            Body=record_json)
 
 
-class GCPVisualizerUploader(Exporter):
-    """
-    Uploads record to visualizer bucket in Google Cloud Storage.
-    """
+# class GCPVisualizerUploader(Exporter):
+#     """
+#     Uploads record to visualizer bucket in Google Cloud Storage.
+#     """
 
-    def __init__(
-        self,
-        project: str,
-        bucket_name: str,
-        region_name: str
-    ) -> None:
-        from faas_profiler_core.storage import GCPRecordStorage
-        self.record_storage = GCPRecordStorage(
-            project=project,
-            bucket_name=bucket_name,
-            region_name=region_name)
+#     def __init__(
+#         self,
+#         project: str,
+#         bucket_name: str,
+#         region_name: str
+#     ) -> None:
+#         from faas_profiler_core.storage import GCPRecordStorage
+#         self.record_storage = GCPRecordStorage(
+#             project=project,
+#             bucket_name=bucket_name,
+#             region_name=region_name)
 
-    def export(self, trace_record: dict) -> None:
-        """
-        Uploads record as json to bucket.
-        """
-        self.record_storage.store_unprocessed_record(trace_record)
+#     def export(self, trace_record: dict) -> None:
+#         """
+#         Uploads record as json to bucket.
+#         """
+#         self.record_storage.store_unprocessed_record(trace_record)
