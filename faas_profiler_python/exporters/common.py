2218822fe119d961d7eead57a8b4446330a683d8
Malte Wächter <git@malte-waechter.com>
2022-09-30 11:12:48 +0200

    Add GCP exporter

--- faas_profiler_python/exporters/common.py
+++ faas_profiler_python/exporters/common.py
@@ -17,27 +17,3 @@ class Console(Exporter):
         Prints raw data to std out.
         """
         print(trace_record)
-
-
-# class GCPVisualizerUploader(Exporter):
-#     """
-#     Uploads record to visualizer bucket in Google Cloud Storage.
-#     """
-
-#     def __init__(
-#         self,
-#         project: str,
-#         bucket_name: str,
-#         region_name: str
-#     ) -> None:
-#         from faas_profiler_core.storage import GCPRecordStorage
-#         self.record_storage = GCPRecordStorage(
-#             project=project,
-#             bucket_name=bucket_name,
-#             region_name=region_name)
-
-#     def export(self, trace_record: dict) -> None:
-#         """
-#         Uploads record as json to bucket.
-#         """
-#         self.record_storage.store_unprocessed_record(trace_record)
